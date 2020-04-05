164eff3b1db6ed4e3b518d9b9f185272eaa3284a
Barun Kumar <barun.kumar04@gmail.com>
2020-04-05 15:35:07 +0530

    Kth Bit Set Or Not Using Right Shift Operator

--- src/com/practice/bitwise/KthBitSetOrNotUsingRightShiftOperator.java
+++ src/com/practice/bitwise/KthBitSetOrNotUsingRightShiftOperator.java
@@ -7,24 +7,24 @@ package com.practice.bitwise;
  * @purpose Find if Kth bit of a number in set or not
  * 
  * @steps 
- *  1. Form a reseter of all bits except for Kth bit. 
- *  2. Apply inputN & bit reseter.
+ *  1. Right shift the input number with (k-1), idea is to shift the Kth bit to last. 
+ *  2. Do inputN & 1.
  *  3. If output is 1 -> bit was not set, otherwise bit is set.
  */
 
 
-public class KthBitSetOrNotUsingLeftShiftOperator {
+public class KthBitSetOrNotUsingRightShiftOperator {
 
 	public static void main(String[] args) {
 		
-		int inputN = 5;
+		int inputN = 13;
 		int k = 3;
 		
 		//step#1 & 2
-		int reseter = 1 << (k-1);
+		int bitShiftedNumber = inputN >> (k-1);
 		
 		//step #3
-		if((inputN & reseter) > 0)
+		if((bitShiftedNumber & 1) > 0)
 			System.out.println("Kth bit is 1.");
 		else
 			System.out.println("Kth bit is 0.");

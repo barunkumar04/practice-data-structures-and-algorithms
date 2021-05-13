376823929c3ebe009c6147e5f27f5d2f9ce2d477
Barun Kumar <barun.kumar04@gmail.com>
2021-05-13 13:30:46 +0530

    Comments enriched

--- src/com/practice/problemsolving/BeggarsOutsideTemple.java
+++ src/com/practice/problemsolving/BeggarsOutsideTemple.java
@@ -11,13 +11,13 @@ public class BeggarsOutsideTemple {
 		 
 		 ArrayList<ArrayList<Integer>> devotees = new ArrayList<ArrayList<Integer>>();
 		 
-		 donation =  new ArrayList<Integer>(Arrays.asList(1,2,10));// Amount 10 to beggars 1 to 2
+		 donation =  new ArrayList<Integer>(Arrays.asList(1,2,10));// Amount 10 to beggars sitting on 1 to 2 positions
 		 devotees.add(donation);
 		 
-		 donation =  new ArrayList<Integer>(Arrays.asList(2,3,20));// Amount 20 to beggars 2 to 3
+		 donation =  new ArrayList<Integer>(Arrays.asList(2,3,20));// Amount 20 to beggars sitting on 2 to 3 positions
 		 devotees.add(donation);
 		 
-		 donation =  new ArrayList<Integer>(Arrays.asList(2,5,25));// Amount 20 to beggars 2 to 3
+		 donation =  new ArrayList<Integer>(Arrays.asList(2,5,25));// Amount 20 to beggars sitting on 2 to 3 positions
 		 devotees.add(donation);
 		 
 		 ArrayList<Integer> beggarsCollection  = calculateBeggerCollections(noOfBeggers, devotees);
@@ -27,7 +27,8 @@ public class BeggarsOutsideTemple {
 
 	private static ArrayList<Integer> calculateBeggerCollections(int noOfBeggers,
 			ArrayList<ArrayList<Integer>> devotees) {
-
+		
+		// Start of day, hence there is no collection to any beggar
 		ArrayList<Integer> beggerCollections = new ArrayList<Integer>();
 		for(int i=0; i<noOfBeggers; i++) {
 			beggerCollections.add(0);
@@ -35,13 +36,15 @@ public class BeggarsOutsideTemple {
 		
 		
 		for(ArrayList<Integer> beggarsAndDonation : devotees) {
-			int amount = beggarsAndDonation.get(2);
+			int amount = beggarsAndDonation.get(2); 
 			int beggarStartIndex = beggarsAndDonation.get(0)-1;
 			int beggarEndIndex = beggarsAndDonation.get(1)-1;
 			
 			beggerCollections.set(beggarStartIndex, beggerCollections.get(beggarStartIndex) + amount);
-			if((beggarEndIndex+1) == beggerCollections.size())
+			// If there is no beggar after end index then no need to negate the contribution
+			if((beggarEndIndex+1) == noOfBeggers)
 				continue;
+			//Putting negative contribution to beggers after the end index
 			beggerCollections.set(beggarEndIndex+1, beggerCollections.get(beggarEndIndex+1) + (amount * -1));
 			
 		}

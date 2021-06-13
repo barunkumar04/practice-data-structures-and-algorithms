140a84e1ae6b093ef643d38aa28f660093dfc083
Barun Kumar <barun.kumar04@gmail.com>
2021-06-13 22:01:45 +0530

    Trap rain water

---
+++ src/com/practice/array/TrappingRainWater.java
@@ -0,0 +1,42 @@
+package com.practice.array;
+
+public class TrappingRainWater {
+
+	public static void main(String[] args) {
+		int[] input = new int[] {2,0,2};
+		System.out.println(trappedWaterVolumn(input));
+	}
+	 public static int trappedWaterVolumn(int[] height) {
+	        //Finding max height
+	        int maxHeight = Integer.MIN_VALUE;
+	        int maxHeightIndex = -1;
+	        for(int i=0; i<height.length; i++){
+	            if(height[i] > maxHeight){
+	                maxHeight = height[i];
+	                maxHeightIndex = i;
+	            }
+	        }
+	        int waterVolumn = 0;
+	        //Iterate till maxHeightIndex, consider this as right max and calcualte water volumn from laft side of buildings
+	        int leftMax = Integer.MIN_VALUE;
+	        for(int i=0; i<maxHeightIndex; i++){
+	            if(height[i] >  leftMax){
+	                leftMax = height[i]; 
+	            }
+	            
+	            waterVolumn += Math.min(leftMax, maxHeight) - height[i];
+	        }
+	       
+	        //calculate for right side of maxHeightIndex
+	        int rightMax = Integer.MIN_VALUE;
+	        for(int i=height.length-1; i>maxHeightIndex; i--){
+	             if(height[i] >=  rightMax){
+	                rightMax = height[i]; 
+	            }
+	            
+	            waterVolumn += Math.min(rightMax, maxHeight) - height[i];
+	        }
+	        
+	        return waterVolumn;
+	    }
+}

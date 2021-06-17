446e9604864ba2fdd2b09c41cc3dcfddebb87296
Barun Kumar <barun.kumar04@gmail.com>
2021-06-17 10:28:10 +0530

    GCD optimized

--- src/com/practice/math/GCD.java
+++ src/com/practice/math/GCD.java
@@ -2,7 +2,7 @@ package com.practice.math;
 
 
 /**
- * Approach - Chinese Mathemetician technique (https://en.wikipedia.org/wiki/Euclidean_algorithm)
+ * Approach - 
  * 1. Use gcd(a,b) = gcd(b, a-b)
  * 2. Use recursive approach
  * 3. Swap a and b so that (a-b) doesnot returns negetive value 
@@ -13,14 +13,14 @@ package com.practice.math;
  *  - Now, gcd(a,b) can be written as gcd(b+k, b)
  *  - Lets assume the gcd(a,b) = g. then g will be the gcd of (b+k, b) as well
  *  - Meaning, a, b and (b+k) is divisible by k
- *  - I.e. k is also divisble by g adn k = a -b
+ *  - I.e. k is also divisible by g and k = a -b
  * 
  ***/
 
 public class GCD {
 
 	public static void main(String[] args) {
-		System.out.println((gcd(5, 10)));
+		System.out.println((gcd(100, 3)));
 
 	}
 	
@@ -30,7 +30,7 @@ public class GCD {
             return A;
         }
         
-        //Swaping to avoid negetive calculation
+        //Swapping to avoid negative calculation
         if(A < B){
             int tmp = A;
             A = B;
@@ -40,3 +40,17 @@ public class GCD {
         return gcd(B, A-B);
     }
 }
+
+/***
+
+TC - O(a/b)
+
+Note, if b = 1(or a smaller b) then TC will be O(a). Which is again not much optimized
+Example: gcd(100, 3) -> gcd(3, 97) -> gcd(3, 94) -> gcd(3, 91) -> .... -> gcd(3, 4) -> gcd(3, 1) -> gcd(2, 1) -> gcd(1, 1) -> gcd(1, 0) - > 1 is the answer.
+
+We can optimize this by doing % operation and avoid method calls from gcd(3, 97) -> gcd(3, 94) -> gcd(3, 91) -> .... -> gcd(3, 4) . 
+
+Proof: A number N can be written as N = k mod some_constant + r. In above approach we avoided k steps
+
+Refer GCDOptimized class for code.
+**/
---
+++ src/com/practice/math/GCDOptimized.java
@@ -0,0 +1,44 @@
+package com.practice.math;
+
+
+/**
+ * Refer GCD class before proceeding.
+ * 
+ * This approach is called Euclid's method
+ ***/
+
+public class GCDOptimized {
+
+	public static void main(String[] args) {
+		System.out.println((gcd(100, 3)));
+
+	}
+	
+    public static int gcd(int A, int B) {
+    	//System.out.println(A +"\t"+ B);
+        //Base check
+        if(B == 0){
+            return A;
+        }
+        
+        //Swapping to avoid negative calculation
+        if(A < B){
+            int tmp = A;
+            A = B;
+            B = tmp;
+        }
+        
+        return gcd(B,A%B);
+    }
+}
+
+/***
+
+TC: O(log max(a,b))
+
+Proof:  
+	- After the first call, gcd(a, b) -> gcd(b, a%b)
+	- Since b < a, then maximum possible value of expression a%b can return is less than a/2	
+	- Similarly, In next call we will reduce b by its half.
+	- So, in every call we are reducing values by its half.
+**/

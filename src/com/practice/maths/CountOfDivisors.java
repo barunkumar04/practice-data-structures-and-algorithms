73884241f9db13ffa69b347ed257502d6d2c215c
Barun Kumar <barun.kumar04@gmail.com>
2021-06-18 22:12:46 +0530

    Divisor count

--- src/com/practice/maths/CountOfDivisors.java
+++ src/com/practice/maths/CountOfDivisors.java
@@ -5,48 +5,42 @@ import java.util.Arrays;
 
 /**
  * Problem Description
- * <p>
  * Given an array of integers A, find and return the count of divisors of each element of the array.
- * <p>
  * NOTE: Order of the resultant array should be same as the input array.
- * <p>
+ * <p/>
  * Problem Constraints
- * 1 <= length of the array <= 100000
- * 1 <= A[i] <= 106
- * <p>
+ *      1 <= length of the array <= 100000
+ *      1 <= A[i] <= 106
+ * <p/>
  * Input Format
- * The only argument given is the integer array A.
- * <p>
+ *      The only argument given is the integer array A.
+ * <p/>
  * Output Format
- * Return the count of divisors of each element of the array in the form of an array.
- * <p>
+ *      Return the count of divisors of each element of the array in the form of an array.
+ * <p/>
  * Example Input
- * Input 1:
- * <p>
- * A = [2, 3, 4, 5]
- * Input 2:
- * <p>
- * A = [8, 9, 10]
- * <p>
- * <p>
+ * <p/>
+ *  Input 1:
+ *      A = [2, 3, 4, 5]
+ * <p/>
+ *  Input 2:
+ *      A = [8, 9, 10]
+ * <p/>
  * Example Output
- * Output 1:
- * <p>
- * [2, 2, 3, 2]
- * Output 1:
- * <p>
- * [4, 3, 4]
- * <p>
- * <p>
+ * <p/>
+ *  Output 1:
+ *      [2, 2, 3, 2]
+ * <p/>
+ *  Output 1:
+ *      [4, 3, 4]
+ * <p/>
  * Example Explanation
- * Explanation 1:
- * <p>
- * The number of divisors of 2 : [1, 2], 3 : [1, 3], 4 : [1, 2, 4], 5 : [1, 5]
- * So the count will be [2, 2, 3, 2].
- * Explanation 2:
- * <p>
- * The number of divisors of 8 : [1, 2, 4, 8], 9 : [1, 3, 9], 10 : [1, 2, 5, 10]
- * So the count will be [4, 3, 4].
+ *      Explanation 1:
+ *          The number of divisors of 2 : [1, 2], 3 : [1, 3], 4 : [1, 2, 4], 5 : [1, 5]
+ *          So the count will be [2, 2, 3, 2].
+ *      Explanation 2:
+ *          The number of divisors of 8 : [1, 2, 4, 8], 9 : [1, 3, 9], 10 : [1, 2, 5, 10]
+ *          So the count will be [4, 3, 4].
  */
 public class CountOfDivisors {
 
